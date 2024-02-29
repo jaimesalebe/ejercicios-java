36c5ee9e232d1871bacb806f122057fb533e8d88
Jaime Salebe <jaimesalebe@outlook.com>
2024-02-29 17:06:32 -0500

    add: allow all cors

---
+++ crud-mysql/src/main/java/com/jaimesalebe/crudmysql/Configuration/WebConf.java
@@ -0,0 +1,16 @@
+package com.jaimesalebe.crudmysql.Configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConf implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // Permitir solicitudes desde cualquier origen
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // Métodos HTTP permitidos
+                .allowedHeaders("*"); // Permitir cualquier encabezado
+    }
+}
